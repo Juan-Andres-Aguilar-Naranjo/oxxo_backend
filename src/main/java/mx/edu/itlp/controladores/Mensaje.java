cbc05116eb12e7660cabb8a74ad76c508c68d4e9
Juan Andrés Aguilar Naranjo <andrescbtis84@gmail.com>
2021-05-03 10:51:13 -0500

    Configuracion URL de BD y nuevo controlador /devops/hola

---
+++ src/main/java/mx/edu/itlp/controladores/Mensaje.java
@@ -0,0 +1,16 @@
+package mx.edu.itlp.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devops")
+class Mensaje {
+
+	@GetMapping("/hola")
+	public String mostrarHola() {
+		return "Hola DevOps"; 
+	}
+	
+}
